23ec49e8cd5f2c0e119fbac611d39bfdf59e62b0
Nodetea <lk201330912@outlook.com>
2017-02-17 10:51:21 +0800

    添加项目文件。

---
+++ Xianqu.Models/Goods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Xianqu.Models
+{
+    class Goods
+    {
+        [Key]
+        [Required]
+        public string GoodsId { get; set; }
+        [Required]
+        public string GoodsCategory { get; set; }
+        [Required]
+        public string GoodsName { get; set; }
+        [Required]
+        public string GoodsDescription { get; set; }
+    }
+}
---
+++ Xianqu.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Xianqu.Web.Controllers
+{
+    public class HomeController : Controller
+    {   
+        [RequireHttps]
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+       public ActionResult Tutorial( ) {
+            ViewBag.Message="这是教程";
+            return View();
+        }
+
+        //public ActionResult
+    }
+}
